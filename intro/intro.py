e90682d3bee6444ea89c0da0ff27fbf2bad74571
Mikel Arina Marcos <mikel.arina.marcos@gmail.com>
2023-03-06 21:52:59 +0100

    first project

--- intro/intro.py
+++ intro/intro.py
@@ -4,3 +4,22 @@ print(9 - 5)
 print(((1 + 3) * (9 - 2) / 2) ** 2)
 # Multiply 3 by 2
 print(3 * 2)
+test_var = 4 + 5
+print(test_var)
+# Set the value of a new variable to 3
+my_var = 3
+# Print the value assigned to my_var
+print(my_var)
+# Change the value of the variable to 100
+my_var = 100
+# Print the new value assigned to my_var
+print(my_var)
+# Create variables
+num_years = 4
+days_per_year = 365
+hours_per_day = 24
+misn_per_hour = 60
+secs_per_min = 60
+# Calculate number of secons in four years
+total_secs = secs_per_min * misn_per_hour * hours_per_day * days_per_year * num_years
+print(total_secs)
